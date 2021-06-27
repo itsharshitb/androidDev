67d53bfd6bcdc26c916f069bbfe7ac9a188b6f2a
Harshit Bhatt <harshitbhatt972@gmail.com>
2021-06-27 12:08:18 +0530

    Update MainActivity.java

--- Weatherpractice/app/src/main/java/com/example/weatherpractice/MainActivity.java
+++ Weatherpractice/app/src/main/java/com/example/weatherpractice/MainActivity.java
@@ -1,34 +1,23 @@
- package com.example.weatherpractice;
-// api key: 4a91b112d83a2c5fdb11d3347cd4dd9f
-import androidx.appcompat.app.AppCompatActivity;
+package com.example.weatherpractice;
 
-import android.content.Context;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -43,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
         String city = cityname.getText().toString();
         RequestQueue requestQueue;
         requestQueue = Volley.newRequestQueue(this);
-       JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET,
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET,
                 "https://api.openweathermap.org/data/2.5/weather?q=london&appid=4a91b112d83a2c5fdb11d3347cd4dd9f", null,
                 new Response.Listener<JSONObject>() {
 
@@ -62,7 +51,7 @@ public class MainActivity extends AppCompatActivity {
                                 resultout.setText(main+":"+desc);
                             }
                         } catch (JSONException e) {
-                          //  resultout.setText("Please select valid location");
+                            //  resultout.setText("Please select valid location");
                             e.printStackTrace();
                         }
                     }
@@ -80,20 +69,20 @@ public class MainActivity extends AppCompatActivity {
                 try {
                     JSONObject arr = response.getJSONObject("main");
 
-                        String temp = arr.getString("feels_like");
-                        String pres = arr.getString("pressure");
-                        String humidity = arr.getString("humidity");
-                        feeling.setText("Temperature: " + temp+"\r\n"+ "Pressure: "+pres+"\r\n"+"Humidity: "+humidity);
+                    String temp = arr.getString("feels_like");
+                    String pres = arr.getString("pressure");
+                    String humidity = arr.getString("humidity");
+                    feeling.setText("Temperature: " + temp+"\r\n"+ "Pressure: "+pres+"\r\n"+"Humidity: "+humidity);
 
                 } catch (JSONException e) {
-                  //  resultout.setText("Please select valid location");
+                    //  resultout.setText("Please select valid location");
                     e.printStackTrace();
                 }
             }
         }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-               // resultout.setText("Please select valid location");
+                // resultout.setText("Please select valid location");
                 Log.i("Exception","Inside 2nd api fetch");
             }
         });
@@ -111,5 +100,4 @@ public class MainActivity extends AppCompatActivity {
         resultout = findViewById(R.id.resultout);
         feeling = findViewById(R.id.feeling);
     }
-}
 }
