982bd03b2e0d7a2f863524104a815224e8fbfea7
Harshit Bhatt <harshitbhatt972@gmail.com>
2021-06-27 12:05:15 +0530

    working on UI

--- Weatherpractice/app/src/main/java/com/example/weatherpractice/MainActivity.java
+++ Weatherpractice/app/src/main/java/com/example/weatherpractice/MainActivity.java
@@ -1,34 +1,23 @@
- package com.example.weatherpractice;
-// api key: 4a91b112d83a2c5fdb11d3347cd4dd9f
-import androidx.appcompat.app.AppCompatActivity;
+package com.example.weatherpractice;
 
-import android.content.Context;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -51,58 +40,56 @@ public class MainActivity extends AppCompatActivity {
                     public void onResponse(JSONObject response) {
                         try {
 //                            Log.i("response", response.getString("weather"));
-                          JSONArray arr = new JSONArray(response.getString("weather"));
-                          for(int i=0;i<arr.length();i++)
-                          {
-                              JSONObject obj = arr.getJSONObject(i);
-                              String main=obj.getString("main");
-                              String desc=obj.getString("description");
-//                              Log.i("weather", desc + "  " + main);
-                              resultout.setText(main+":"+desc);
-                          }
+                            JSONArray arr =response.getJSONArray("weather");
+                            for(int i=0;i<arr.length();i++)
+                            {
+                                JSONObject obj = arr.getJSONObject(i);
+                                String main=obj.getString("main");
+                                Log.i("info",main);
+                                String desc=obj.getString("description");
+                                Log.i("info",desc);
+                                resultout.setText(main+":"+desc);
+                            }
                         } catch (JSONException e) {
-                            resultout.setText("Please select valid location");
+                            //  resultout.setText("Please select valid location");
                             e.printStackTrace();
                         }
                     }
                 }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                resultout.setText("Please select valid location");
-                Log.i("Exception","Inside first api fetch");
+
             }
+
         });
 
-        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET,
-                "https://api.openweathermap.org/data/2.5/weather?q=london&appid=4a91b112d83a2c5fdb11d3347cd4dd9f", null, new Response.Listener<JSONArray>() {
-            @Override
-            public void onResponse(JSONArray response) {
+        JsonObjectRequest jsonObjectRequest1 = new JsonObjectRequest(Request.Method.GET,
+                "https://api.openweathermap.org/data/2.5/weather?q=london&appid=4a91b112d83a2c5fdb11d3347cd4dd9f", null, new Response.Listener<JSONObject>() {
+            public void onResponse(JSONObject response) {
                 try {
-                    JSONArray arr = new JSONArray(response.getString(Integer.parseInt("main")));
-                    for(int i=0;i<arr.length();i++)
-                    {
-                        JSONObject obj = arr.getJSONObject(i);
-                        String temp = obj.getString("feels_like");
-                        String pres = obj.getString("pressure");
-                        String humidity = obj.getString("humidity");
-                        feeling.setText("Temperature: " + temp+"\r\n"+ "Pressure: "+pres+"\r\n"+"Humidity: "+humidity);
-                    }
+                    JSONObject arr = response.getJSONObject("main");
+
+                    String temp = arr.getString("feels_like");
+                    String pres = arr.getString("pressure");
+                    String humidity = arr.getString("humidity");
+                    feeling.setText("Temperature: " + temp+"\r\n"+ "Pressure: "+pres+"\r\n"+"Humidity: "+humidity);
+
                 } catch (JSONException e) {
-                    resultout.setText("Please select valid location");
+                    //  resultout.setText("Please select valid location");
                     e.printStackTrace();
                 }
             }
         }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                resultout.setText("Please select valid location");
+                // resultout.setText("Please select valid location");
                 Log.i("Exception","Inside 2nd api fetch");
             }
         });
 
 
         requestQueue.add(jsonObjectRequest);
-        requestQueue.add(jsonArrayRequest);
+        requestQueue.add(jsonObjectRequest1);
     }
 
     @Override
